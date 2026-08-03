da94d1a04fca236cb22a2ed18058e9fb0c997f9d
CyberBass051 <pb05129000@tutamail.com>
2026-08-04 01:50:06 +0200

    fix: SG Description format fix

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -147,7 +147,7 @@ resource "aws_security_group_rule" "db_ingress_from_proxy" {
   to_port                  = 5432
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.proxy.id
-  description              = "Allow ingress from RDS Proxy only — nothing else reaches the DB"
+  description              = "Allow ingress from RDS Proxy only nothing else reaches the DB"
 }
 
 resource "aws_security_group_rule" "lambda_egress_to_vpce" {
