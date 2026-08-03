cc55478cf39de18a8d77d0aabfe377f7a3740c80
CyberBass051 <pb05129000@tutamail.com>
2026-08-04 01:56:33 +0200

    fix: add VPC flow logs, Checkov scan exception, docs/adr/security-group-hardening.md

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -202,3 +202,53 @@ resource "aws_vpc_endpoint" "logs" {
   }
 }
  
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc/${var.project_name}-flow-logs"
+  retention_in_days = 14
+
+  tags = {
+    Name = "${var.project_name}-vpc-flow-logs"
+  }
+}
+
+resource "aws_iam_role" "flow_logs" {
+  name = "${var.project_name}-vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "flow_logs" {
+  name = "${var.project_name}-vpc-flow-logs-policy"
+  role = aws_iam_role.flow_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ]
+      Resource = "${aws_cloudwatch_log_group.vpc_flow_logs.arn}:*"
+    }]
+  })
+}
+
+resource "aws_flow_log" "main" {
+  vpc_id                   = aws_vpc.main.id
+  traffic_type              = "ALL"
+  log_destination_type      = "cloud-watch-logs"
+  log_destination           = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  iam_role_arn              = aws_iam_role.flow_logs.arn
+}
