ab53d87af9212e4d23717ec422efb97ed439ca2a
CyberBass051 <pb05129000@tutamail.com>
2026-08-04 23:42:35 +0200

    fix: CKV2_AWS_64 CKV2_AWS_158

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -207,6 +207,39 @@ resource "aws_kms_key" "flow_logs" {
   deletion_window_in_days = 7
   enable_key_rotation     = true
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "EnableRootAccountAccess"
+        Effect    = "Allow"
+        Principal = { AWS = "arn:aws:iam::221717898536:root" }
+        Action    = "kms:*"
+        Resource  = "*"
+      },
+      {
+        Sid    = "AllowCloudWatchLogsEncryption"
+        Effect = "Allow"
+        Principal = {
+          Service = "logs.us-east-1.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt*",
+          "kms:Decrypt*",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:Describe*"
+        ]
+        Resource = "*"
+        Condition = {
+          ArnLike = {
+            "kms:EncryptionContext:aws:logs:arn" = "arn:aws:logs:us-east-1:221717898536:*"
+          }
+        }
+      }
+    ]
+  })
+
   tags = {
     Name = "${var.project_name}-flow-logs-kms"
   }
@@ -220,6 +253,7 @@ resource "aws_kms_alias" "flow_logs" {
 resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
   name              = "/aws/vpc/${var.project_name}-flow-logs"
   retention_in_days = 14
+  kms_key_id        = aws_kms_key.flow_logs.arn
 
   tags = {
     Name = "${var.project_name}-vpc-flow-logs"
