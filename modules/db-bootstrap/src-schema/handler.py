77ee445ac1880e4b25b0520830feeab56a5181c8
CyberBass051 <pb05129000@tutamail.com>
2026-08-09 00:20:11 +0200

    fix: lambda handler

--- modules/db-bootstrap/src-schema/handler.py
+++ modules/db-bootstrap/src-schema/handler.py
@@ -5,16 +5,18 @@ import psycopg2
 
 secrets_client = boto3.client("secretsmanager")
 
-def lambda_handler(context, event):
-    print(f"Fetching secret from {os.environ["MASTER_SECRET"]}")
+def lambda_handler(event, context):
+    secret_arn = os.environ["MASTER_SECRET"]
+    print(f"Fetching secret from {secret_arn}")
     secret = json.loads(
-        secrets_client.get_secret_value(SecretId=os.environ["MASTER_SECRET"])["SecretString"]
+        secrets_client.get_secret_value(SecretId=secret_arn)["SecretString"]
     )
     print("Secret retrieved. Attempting DB connection...")
+
     try:
         conn = psycopg2.connect(
             host=os.environ["DB_HOST"],
-            dbName=os.environ["DB_NAME"],
+            dbname=os.environ["DB_NAME"],
             user=secret["username"],
             password=secret["password"],
             sslmode="require",
